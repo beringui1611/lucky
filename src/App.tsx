890221a2bd3414f2957dc4567fcf1762f198bfa3
beringui1611 <fortiniteberingui12@gmail.com>
2024-12-13 17:57:29 -0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -119,10 +119,15 @@ function App() {
             <p className="text-white text-3xl">
               LCK ${formatLargeNumber(Number(supply))} / 200M
             </p>
-            <hr className="border-2 border-medium w-[90%]" />
-            <div className="flex gap-2 text-sm text-slate-400">
-              <label>Current: ${currentPrice.toFixed(2)}</label>
-              <label>Next: ${(currentPrice + 0.01).toFixed(2)}</label>
+            <div className="border-2 border-medium w-[90%] mb-2 rounded-md" >
+            <div className="flex gap-2 text-sm text-slate-400 bg-medium bg-opacity-30 p-1" style={{width: `${supply}%`}}>
+              <div className="flex w-screen justify-between">
+                <label className="ml-5 md:ml-16">Current: ${currentPrice.toFixed(2)}K</label>
+                <label className="ml-5 md:ml-36">Next: ${(currentPrice + 0.01).toFixed(2)}/100K</label>
+                <label className="ml-5 md:ml-36">Next: ${(currentPrice + 0.02).toFixed(2)}/1M</label>
+                <label className="ml-5 md:ml-36">Next: ${(currentPrice + 0.03).toFixed(2)}/10M</label>
+              </div>
+            </div>
             </div>
           </div>
 
